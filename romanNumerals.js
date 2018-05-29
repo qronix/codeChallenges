77b86eddf54fe39a645c371d7a8dcf8eba167148
Qronix <jonhead7240@gmail.com>
2018-05-28 20:14:55 -0400

    Cleaned code

--- romanNumerals.js
+++ romanNumerals.js
@@ -10,21 +10,6 @@ var romNums = {
 var outputStr = " ";
 
 class RomanNumerals {
-
-
-	constructor() {
-		romNums = {
-			'I': 1,
-			'V': 5,
-			'X': 10,
-			'L': 50,
-			'C': 100,
-			'D': 500,
-			'M': 1000
-		};
-		outputStr = " ";
-	}
-
 	static toRoman(num) {
 		outputStr = " ";
 		var strNum = num.toString();
